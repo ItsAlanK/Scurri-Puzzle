0a63ec9e8ed9f9675c91aad9db958a472921b753
Alan Kinsella <alankinsella1993@gmail.com>
2024-02-28 16:44:54 +0000

    Add docstring to postcode formatter validator

--- postcode_formatter.py
+++ postcode_formatter.py
@@ -10,6 +10,14 @@ import re
 #     print("Invalid postcode")
 
 class PostcodeFormatter:
+    """
+    Check if postcode provided is valid according to UK postcode rules.
+    Rules found at: https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation
+    Args:
+        postcode: str
+    Returns:
+        bool: True if postcode is valid, False if not.
+    """
     def is_valid(postcode):
         regex = r'^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$'
         return re.match(regex, postcode) is not None
